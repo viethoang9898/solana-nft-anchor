aab2e4834719a61ab4c6d421e2c96267049ffa5e
viethoang9898 <153875716+viethoang9898@users.noreply.github.com>
2023-12-15 22:15:14 +0700

    declare program id

--- programs/solana-nft-anchor/src/lib.rs
+++ programs/solana-nft-anchor/src/lib.rs
@@ -12,7 +12,7 @@ use mpl_token_metadata::{
     state::DataV2,
 };
 
-declare_id!("<UPDATE HERE>");
+declare_id!("9pNhpTZyRZztF5Yh1JanPRqUDxoufyt73E9RPFBCybNW");
 #[program]
 pub mod solana_nft_anchor {
 
